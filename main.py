28fbfa8701f16ed9c0da46c95a245d7639010cd9
Dalton <daltons@berkeley.edu>
2021-11-17 17:46:37 -0800

    Fixed square declaration

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import random
 import tkinter as tk
+import time
+import math
 
 
 class Square:
     def __init__(self, row, col, parent_game):
-        self.seen = ' '
-        self.real = 0
-        self.flagged = False
         self.row = row
         self.col = col
         self.game = parent_game
+        self.seen = ' '
+        self.real = 0
+        self.flagged = False
         self.canvas = tk.Canvas(master=game_frame, width=25, height=25)
         self.canvas.create_image(0, 0, image=get(' '), anchor=tk.NW)
         self.canvas.bind("<1>", self.left_click)
@@ -23,8 +25,10 @@ class Square:
 
 
 class Game:
-    def __init__(self, difficulty):
-        self.difficulty = difficulty
+    def __init__(self, diff):
+
+        # Set difficulty
+        self.difficulty = diff
         if self.difficulty == "Beginner":
             self.height = 9
             self.width = 9
@@ -37,25 +41,38 @@ class Game:
             self.height = 16
             self.width = 30
             self.num_mines = 99
+
+        # instantiate board
         self.board = [[Square(row, col, self) for col in range(self.width)] for row in range(self.height)]
+
         self.filled = False
         self.vis = []
         self.flags = 0
         self.game_over = False
 
     def left_click(self, row, col):
+        # If game is over, do nothing
         if self.game_over:
             return
+
         square = self.board[row][col]
+
+        # If square is flagged, do nothing
         if square.flagged:
             return
+
+        # If square is revealed, reveal neighbors if fully flagged
         if square.seen != ' ':
             if self.check_full(row, col):
                 self.reveal_neighbors(row, col)
+
+        # Fill board if haven't already
         elif not self.filled:
             self.fill_board(row, col)
             self.set_nums()
             self.filled = True
+            self.game_time = time.time()
+            self.update_clock()
         self.reveal(row, col)
         self.check_over()
 
@@ -76,26 +93,36 @@ class Game:
             self.reveal(neighbour.row, neighbour.col)
 
     def right_click(self, row, col):
+        # If game is over, do nothing
         if self.game_over:
             return
         square = self.board[row][col]
+        # If game hasn't started, do nothing
         if not self.filled:
             return
+        # If already flagged, remove flag
         if square.flagged:
             square.flagged = False
             self.flags -= 1
+            mines_label["text"] = str(self.num_mines - self.flags)
             self.set_seen(row, col, ' ')
+        # If square is already revealed, do nothing
         elif square.seen != ' ':
             return
+        # If you haven't flagged too many, flag the square
         elif self.flags < self.num_mines:
             square.flagged = True
             self.flags += 1
+            mines_label["text"] = str(self.num_mines - self.flags)
             self.set_seen(row, col, 'f')
 
+    # Reveal a square
     def reveal(self, row, col):
         square = self.board[row][col]
+        # If empty space, reveal neighbours
         if square.real == 0:
             self.neighbours(row, col)
+        # If mine and not flagged, game over
         elif square.real == -1:
             if not square.flagged:
                 self.show_mines()
@@ -219,18 +246,36 @@ class Game:
 
     def show_mines(self):
         self.game_over = True
+        window.after_cancel(self.timer_job)
         for row in range(self.height):
             for col in range(self.width):
                 if self.board[row][col].real == -1:
                     self.set_seen(row, col, -1)
 
     def build_gui(self):
+        global mines_label
+        global time_label
+
+        # Build the mines label
+        mines_label = tk.Label(master=control_frame, text=str(self.num_mines))
+        mines_label.config(font=("Impact", 44))
+        mines_label.pack()
+
+        time_label = tk.Label(master=control_frame, text="000")
+        time_label.config(font=("Impact", 44))
+        time_label.pack()
+
+        # Configure the mine grid
         game_frame.columnconfigure([x for x in range(self.width)], minsize=25)
         game_frame.rowconfigure([y for y in range(self.height)], minsize=25)
 
         for row in range(self.height):
             for col in range(self.width):
                 self.board[row][col].canvas.grid(row=row, column=col)
+
+    def update_clock(self):
+        time_label["text"] = str(math.floor(time.time() - self.game_time))
+        self.timer_job = window.after(1000, self.update_clock)
 
 
 # Referenced https://stackoverflow.com/questions/53861528/runtimeerror-too-early-to-create-image/53861790
@@ -262,6 +307,10 @@ def new_game(event):
     global game_frame
     global game
 
+    mines_label.destroy()
+    time_label.destroy()
+    window.after_cancel(game.timer_job)
+
     game_frame.destroy()
     game_frame = tk.Frame(master=window)
     game_frame.pack()
@@ -276,9 +325,11 @@ if __name__ == '__main__':
 
     control_frame = tk.Frame(master=window)
     control_frame.pack()
+
     new_game_button = tk.Button(master=control_frame, text="New Game", width=10, height=3)
     new_game_button.pack()
     new_game_button.bind("<1>", new_game)
+
     difficulties = ["Beginner", "Intermediate", "Expert"]
     difficulty = tk.StringVar(control_frame)
     difficulty.set(difficulties[2])
